061054b3481a3a1fc633691849717c445fc8ab69
day2 <latha.krishnayya@gmail.com>
2021-12-02 14:35:19 +0100

    JPA-Mapper part 2 finished

---
+++ src/main/java/se/lexicon/sneha/jpa/data/DetailsDAO.java
@@ -0,0 +1,5 @@
+package se.lexicon.sneha.jpa.data;
+
+public interface DetailsDAO {
+
+}
---
+++ src/main/java/se/lexicon/sneha/jpa/model/AppUser.java
@@ -0,0 +1,85 @@
+package se.lexicon.sneha.jpa.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private int appUserId;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private LocalDate regDate;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_user_details_id", table = "app_user")
+    private Details userDetails;
+
+
+    public AppUser(String username, String password, LocalDate regDate, Details userDetails) {
+        this.username = username;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public AppUser() {
+    }
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public Details getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(Details userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return appUserId == appUser.appUserId && Objects.equals(username, appUser.username) && Objects.equals(password, appUser.password) && Objects.equals(regDate, appUser.regDate) && Objects.equals(userDetails, appUser.userDetails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appUserId, username, password, regDate, userDetails);
+    }
+}
